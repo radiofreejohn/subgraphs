df66dc0d5497b39ea2dc6da253487376481da033
John Clover <radiofreejohn@users.noreply.github.com>
2024-02-11 16:49:31 -0500

    initial commit for modified subgraph

---
+++ README.md
@@ -0,0 +1,17 @@
+# Subgraph 101
+
+## Basic subgraph with `graph init`
+
+We'll be indexing an [ERC-721 NFT contract](https://explorer.testnet.immutable.com/token/0xD2c0e3119C67d7A21fFC74383fB10e510F706A45) on Immutable Testnet.
+
+What you'll need for your own:
+- Contract Address
+- ABI
+- Start Block
+
+Caveat: Immutable isn't included in The Graph CLI's list of supported networks so some manual editing is needed.
+
+## Mutable data and aggregates
+
+Much of the code for this example was borrowed from [Benjythebee](https://github.com/Benjythebee/erc721-and-erc1155-subgraph).
+
--- src/guild.ts
+++ src/guild.ts
@@ -1,3 +1,5 @@
+import { Address, BigInt, log } from "@graphprotocol/graph-ts"
+// This is generated from our ABI.
 import {
   Approval as ApprovalEvent,
   ApprovalForAll as ApprovalForAllEvent,
@@ -6,135 +8,163 @@ import {
   RoleAdminChanged as RoleAdminChangedEvent,
   RoleGranted as RoleGrantedEvent,
   RoleRevoked as RoleRevokedEvent,
-  Transfer as TransferEvent
+  Transfer as TransferEvent,
+  guild as Contract
 } from "../generated/guild/guild"
-import {
-  Approval,
-  ApprovalForAll,
-  EIP712DomainChanged,
-  OperatorAllowlistRegistryUpdated,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  Transfer
-} from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+// This is generated from our `schema.graph` file.
+import {Owner, OwnerTokenLookup, Token, TokenContract} from "../generated/schema"
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleEIP712DomainChanged(
-  event: EIP712DomainChangedEvent
-): void {
-  let entity = new EIP712DomainChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOperatorAllowlistRegistryUpdated(
-  event: OperatorAllowlistRegistryUpdatedEvent
-): void {
-  let entity = new OperatorAllowlistRegistryUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.oldRegistry = event.params.oldRegistry
-  entity.newRegistry = event.params.newRegistry
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleAdminChanged(event: RoleAdminChangedEvent): void {
-  let entity = new RoleAdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-  entity.newAdminRole = event.params.newAdminRole
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleGranted(event: RoleGrantedEvent): void {
-  let entity = new RoleGranted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleRoleRevoked(event: RoleRevokedEvent): void {
-  let entity = new RoleRevoked(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.role = event.params.role
-  entity.account = event.params.account
-  entity.sender = event.params.sender
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+import { normalize } from "./helpers";
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  // The names of `params` are derived directly from the provided ABI.
+  let tokenId = event.params.tokenId;
+  // In our example, a custom ID isn't needed, but consider that this would be useful if we were
+  // indexing multiple contracts handling different tokens.
+  let id = event.address.toHex() + '_' + tokenId.toString();
+  let contractId = event.address.toHex();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let contract = Contract.bind(event.address);
 
-  entity.save()
+  // In our mappings, we can read previously stored values, and if they
+  // don't exist, we can create new a new entity to store.
+  let tokenContract = TokenContract.load(contractId);
+  if(tokenContract == null) {
+    tokenContract = new TokenContract(contractId)
+    // See documentation for accessing public state:
+    // https://thegraph.com/docs/en/developing/graph-ts/api/#access-to-smart-contract-state
+    // This generates an `eth_call` which is costly and can take time, so try to limit use.
+    let name = contract.try_name();
+    if(!name.reverted) {
+        // This is a handy function to normalize strings. You don't always know what will end up on-chain,
+        // but certain values like \u0000 can't be stored in the graph-node database (Postgres).
+        tokenContract.name = normalize(name.value);
+    }
+    let symbol = contract.try_symbol();
+    if(!symbol.reverted) {
+        tokenContract.symbol = normalize(symbol.value);
+    }
+    tokenContract.save()
+  }
+  let token = Token.load(id)
+  if(token == null){
+    token = new Token(id)
+    token.contract = tokenContract.id;
+    token.tokenID = tokenId;
+    token.mintTime = event.block.timestamp;
+    let metadataURI = contract.try_tokenURI(tokenId);
+    if(!metadataURI.reverted) {
+        token.tokenURI = normalize(metadataURI.value);
+    } else {
+        token.tokenURI = "";
+    }
+    token.save()
+  }
+
+  // Load owner or save new owner. Ignore "zero" address used to mint.
+  // In reality, we should always have an owner for a token that was previously minted,
+  // reality is often surprising on-chain though!
+  if (event.params.from != Address.zero()) {
+    log.warning("zero address",[])
+
+    let from = Owner.load(event.params.from.toHex())
+    if (from == null) {
+        from = new Owner(event.params.from.toHex())
+        from.save()
+    }
+
+  }
+
+  // Same as above, but checking the to address. "zero" here would be a burn.
+  if (event.params.to != Address.zero()) {
+    let to = Owner.load(event.params.to.toHex())
+    if (to == null){
+        to = new Owner(event.params.to.toHex())
+        to.save()
+    }
+  }
+
+  handleLookupQuantity(contract,event.params.from,event.params.to,token,BigInt.fromI32(1));
+}
+
+function handleLookupQuantity(contract:Contract,from:Address,to:Address,token:Token,quantity:BigInt=BigInt.fromI32('1')):void{
+
+  let wasMinted: boolean = from == Address.zero();
+
+  let isBurned: boolean = to == Address.zero();
+
+  // If it was minted, the FROM address is the Zero address, therefore we don't create a lookup for it. (it won't work)
+  if (wasMinted == false) {
+    // I denote look-ups with the id `UserAddress_ContractAddress_tokenId`
+    let fromLookupId = from.toHex() + "_" + token.id;
+
+    let fromLookup = OwnerTokenLookup.load(fromLookupId);
+    if (fromLookup == null) {
+      // Lookup doesn't exist so we create a new one.
+      fromLookup = new OwnerTokenLookup(fromLookupId);
+      fromLookup.owner = from.toHex();
+      fromLookup.contract = token.contract;
+      fromLookup.token = token.id;
+      fromLookup.quantity = new BigInt(0);
+      fromLookup.save();
+    }
+
+    log.debug("Getting balance of from: {}", [from.toHex()]);
+
+    // We hit the contract of that collection and ask it how much the FROM user owns.
+    let balFrom = contract.try_balanceOf(from);
+
+    if (!balFrom.reverted) {
+      // if contract responded, set the quantity FROM user owns.
+      fromLookup.quantity = balFrom.value;
+    } else if (fromLookup.quantity >= quantity) {
+      // if contract badly responded, we attempt to do simple math and remove the quantity sent.
+      let amount = fromLookup.quantity.toI32() - quantity.toI32();
+      fromLookup.quantity = new BigInt(amount);
+    } else {
+      // Else if the value sent is greater than previous quantity, we set it to 0.
+      fromLookup.quantity = new BigInt(0);
+    }
+    fromLookup.save();
+  }
+
+  // If it was burned, the TO address is the Zero address, therefore we don't create a lookup for it. (it won't work)
+  if (isBurned == false) {
+    // I denote look-ups with the id `UserAddress_contractAddress_tokenId`
+    let toLookupId = to.toHex() + "_" + token.id;
+    // to lookup handler
+    let toLookup = OwnerTokenLookup.load(toLookupId);
+    if (toLookup == null) {
+      toLookup = new OwnerTokenLookup(toLookupId);
+      toLookup.owner = to.toHex();
+      toLookup.contract = token.contract;
+      toLookup.token = token.id;
+      toLookup.quantity = new BigInt(0);
+      toLookup.save();
+    }
+
+    // This collectible was minted and sent to this user, therefore we know the user now owns the full value.
+    if (wasMinted) {
+      toLookup.quantity = quantity;
+    } else {
+      // Else, we hit the contract of this collectible's collection to know how much TO owns.
+      let balTo = contract.try_balanceOf(to);
+
+      if (!balTo.reverted) {
+        // If replied nicely, we set the quantity that user owns for this collectible.
+        toLookup.quantity = balTo.value;
+      } else {
+        // if contract badly responded, we attempt to do simple math and add the quantity received.
+        let amount = toLookup.quantity.toI32() + quantity.toI32();
+        toLookup.quantity = new BigInt(amount);
+      }
+      //Check the quantity is valid:
+      // if (toLookup.quantity == null) {
+      //   toLookup.quantity = new BigInt(1);
+      // }
+    }
+
+    toLookup.save();
+  }
 }
